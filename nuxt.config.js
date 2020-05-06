eb55bc09e771f65016f1f0c862d3327bd4578d19
Mohsen Pakzad <44529676+mohsenpakzad@users.noreply.github.com>
2020-05-06 19:47:53 +0430

    change logo

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/icon.png' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Material+Icons' }
     ]
   },
