db0f46b944593a1d766b7306d3168f4ec65b4d32
Mohsen Pakzad <44529676+mohsenpakzad@users.noreply.github.com>
2020-06-19 06:22:14 +0430

    Create header and move change theme button form index to header

---
+++ components/AppHeader.vue
@@ -0,0 +1,41 @@
+<template>
+
+  <v-app-bar
+    app
+    color="primary"
+    dark
+    flat
+  >
+
+    <v-toolbar-title
+    >
+
+      <v-icon large color="secondary">mdi-account-alert</v-icon>
+
+    </v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn
+      icon
+      color="secondary"
+      @click="toggleTheme"
+    >
+      <v-icon v-if="$vuetify.theme.dark">mdi-white-balance-sunny</v-icon>
+      <v-icon v-else>mdi-weather-night</v-icon>
+    </v-btn>
+
+  </v-app-bar>
+
+</template>
+
+<script>
+  export default {
+    name: "AppHeader",
+    methods: {
+      toggleTheme() {
+        this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+      }
+    }
+  }
+</script>
