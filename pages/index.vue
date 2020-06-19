1e19991846e465e3ec5ce43994197f22aea19342
Mohsen Pakzad <44529676+mohsenpakzad@users.noreply.github.com>
2020-06-19 06:24:55 +0430

    Refactor template code and add some extra information about myself

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,74 @@
 <template>
-  <v-layout
-    column
-  >
-    <v-flex
-      mt-10
-      ms-10
-      xs12
-      sm8
-      md6
-    >
+  <v-container fluid>
 
-      <h1 class="font-weight-medium display-2 py-3">Hello.</h1>
-      <h2 class="font-weight-medium display-1 py-3">My name is Mohsen Pakzad.</h2>
-      <p class="font-weight-regular headline py-4">I'm computer engineer of Shiraz university.</p>
-      <a href="https://github.com/mohsenpakzad" target="_blank">This my github page</a>
-    </v-flex>
-  </v-layout>
+    <v-row
+      class="ma-3"
+    >
+      <v-col
+      >
+
+        <h1
+          class="font-weight-medium display-2 py-3"
+        >
+          Hello.
+        </h1>
+
+        <h2
+          class="font-weight-medium display-1 py-3"
+        >
+          My name is Mohsen Pakzad.
+        </h2>
+
+
+        <!-- TODO: make formattedAge show also months and days -->
+        <h3
+          class="font-weight-medium display-1 py-3"
+        >
+          I born in {{ myFormattedBirthday }}, and currently {{ myFormattedAge }} old.
+        </h3>
+
+        <p
+          class="font-weight-regular headline py-4"
+        >
+          I'm computer engineer of Shiraz university.
+        </p>
+
+        <v-btn
+          outlined
+          href="https://github.com/mohsenpakzad"
+          target="_blank"
+          large
+        >
+
+          <v-icon left>mdi-github</v-icon>
+
+          My github page
+
+        </v-btn>
+
+      </v-col>
+
+    </v-row>
+
+  </v-container>
 </template>
+
+<script>
+import moment from 'moment';
+
+  export default {
+    name: 'index',
+    data: () => ({
+      myBirthday: moment('1999-7-25')
+    }),
+    computed: {
+      myFormattedBirthday(){
+        return this.myBirthday.format('MMMM Do YYYY');
+      },
+      myFormattedAge() {
+        return this.myBirthday.fromNow(true);
+      }
+    },
+  }
+
+</script>
