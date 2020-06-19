e3a0fd20331958d8d39c4ae60c1331c2de5669b5
Mohsen Pakzad <44529676+mohsenpakzad@users.noreply.github.com>
2020-06-19 06:22:42 +0430

    Migrate form v-content to v-main and add header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,26 @@
 <template>
-  <v-app>
-    <v-content>
-      <v-container fluid>
-        <nuxt />
-      </v-container>
-    </v-content>
+  <v-app
+  >
+
+    <app-header></app-header>
+
+    <v-main>
+
+      <nuxt/>
+
+    </v-main>
+
   </v-app>
 </template>
 
+<script>
+  import AppHeader from "../components/AppHeader";
+
+  export default {
+    name: 'default',
+    components: {
+      AppHeader
+    }
+  }
+
+</script>
