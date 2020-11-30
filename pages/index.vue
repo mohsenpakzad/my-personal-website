bfdff59db1507430a5407c6a21211b7361199fe9
Mohsen Pakzad <44529676+mohsenpakzad@users.noreply.github.com>
2020-11-30 06:56:13 +0330

    Refactor all previous WHF codes and create new looking

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,34 @@
 <template>
   <v-container fluid>
-    <v-row class="ma-3">
-      <v-col>
-        <v-avatar class="mb-4" size="250">
-          <v-img src="/me.jpg"></v-img>
-        </v-avatar>
-        <h1 class="font-weight-medium text-h3 py-3">
-          Hello.
-        </h1>
-        <h2 class="font-weight-medium text-h4 py-3">
-          My name is Mohsen Pakzad.
-        </h2>
+    <v-row justify="center">
+      <v-avatar class="my-4" size="280">
+        <v-img src="/me.jpg"></v-img>
+      </v-avatar>
+    </v-row>
+    <h1 class="font-weight-medium text-center text-h3 py-3">
+      Hello
+    </h1>
+    <h2 class="font-weight-medium text-center text-h4 py-3">
+      My name is Mohsen Pakzad
+    </h2>
 
-        <!-- TODO: make formattedAge show also months and days -->
-        <h3 class="font-weight-medium text-h4 py-3">
-          I born in {{ myFormattedBirthday }}, and currently {{ myFormattedAge }} old.
-        </h3>
-        <p class="font-weight-regular headline py-4">
-          I'm computer engineer of Shiraz university.
-        </p>
-        <v-btn
-            outlined
-            :href="myGithubPageLink"
-            target="_blank"
-            large
-        >
-          <v-icon left>mdi-github</v-icon>
-          My github page
-        </v-btn>
-      </v-col>
+    <!-- TODO: make formattedAge show also months and days -->
+    <h3 class="font-weight-medium text-center text-h4 py-3">
+      I born in {{ myFormattedBirthday }}, and currently {{ myFormattedAge }} old
+    </h3>
+    <p class="font-weight-regular text-center headline py-4">
+      I'm currently studying computer engineering at Shiraz university
+    </p>
+    <v-row justify="center">
+      <v-btn
+          outlined
+          :href="myGithubPageLink"
+          target="_blank"
+          large
+      >
+        <v-icon left>mdi-github</v-icon>
+        My github page
+      </v-btn>
     </v-row>
   </v-container>
 </template>
