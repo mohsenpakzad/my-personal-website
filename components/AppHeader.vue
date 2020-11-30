4c151e0da0b718e291083fda6b5b7b81b572b952
Mohsen Pakzad <44529676+mohsenpakzad@users.noreply.github.com>
2020-11-30 05:56:27 +0330

    Remove dark mode feature :(

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -8,25 +8,11 @@
     <v-toolbar-title>
       <v-icon large color="secondary">mdi-account</v-icon>
     </v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-btn
-        icon
-        color="secondary"
-        @click="toggleTheme"
-    >
-      <v-icon v-if="$vuetify.theme.dark">mdi-white-balance-sunny</v-icon>
-      <v-icon v-else>mdi-weather-night</v-icon>
-    </v-btn>
   </v-app-bar>
 </template>
 
 <script>
 export default {
   name: "AppHeader",
-  methods: {
-    toggleTheme() {
-      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
-    }
-  }
 }
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -42,20 +42,10 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: false,
       themes: {
         light: {
           primary: colors.grey.darken4,
           secondary: colors.grey.lighten5,
-          accent: colors.shades.black,
-          error: colors.red.accent3,
-          info: colors.teal.lighten1,
-          success: colors.green.accent4,
-          warning: colors.amber.base
-        },
-        dark: {
-          primary: colors.grey.lighten5,
-          secondary: colors.grey.darken4,
           accent: colors.shades.black,
           error: colors.red.accent3,
           info: colors.teal.lighten1,
